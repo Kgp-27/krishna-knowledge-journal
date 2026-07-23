825096c7511da883cf98fcd9c328a1c99bd1d6a1
Kgp-27 <iamkgp209@gmal.com>
2026-07-23 15:30:32 +0530

    Configure GitHub CMS and update production settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,21 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import keystatic from "@keystatic/astro";
 import cloudflare from "@astrojs/cloudflare";
-
 import react from "@astrojs/react";
-
 import markdoc from "@astrojs/markdoc";
 
 export default defineConfig({
-
   site: "https://theknowledgejournal.com",
 
-adapter: cloudflare(),
+  output: "server",
 
-  integrations: [sitemap(), keystatic(), react(), markdoc()],
+  adapter: cloudflare(),
+
+  integrations: [
+    sitemap(),
+    keystatic(),
+    react(),
+    markdoc(),
+  ],
 
 });
